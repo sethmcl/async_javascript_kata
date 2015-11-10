470703e02d5c41768648fa8dd54ba5a4183315c5
Seth McLaughlin <s@sethmcl.com>
2015-11-10 10:15:17 -0800

    chapter 6 examples

---
+++ chapter_6/examples/app.js
@@ -0,0 +1,4 @@
+var d = document.createElement('div');
+d.innerHTML = '<ul><li>my app!</li></ul>';
+document.body.appendChild(d);
+console.log('%cfinished loading app.js', 'font-size:300%; background:yellow;');
